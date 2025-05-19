6166d2517527cfaa55ac1aa4fe2622792f0336fb
tai.c.bui <tai.c.bui@gmail.com>
2025-05-20 01:36:38 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-//f the PDF is hosted on a different domain, ensure the server allows CORS for your Next.js app. You may need to set the crossOrigin attribute or use a proxy in Next.js.
-
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/external-pdf/:path*',
-        destination: 'https://example.com/:path*',
-      },
-    ];
-  },
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
